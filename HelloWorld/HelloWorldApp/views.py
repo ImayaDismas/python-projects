ec1ffc44050ca1f654fc126e830e984719cf307a
ImayaDismas <imayadismas@gmail.com>
2016-02-26 02:02:54 +0300

    inheritance using templates complete

--- HelloWorld/HelloWorldApp/views.py
+++ HelloWorld/HelloWorldApp/views.py
@@ -1,8 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-def foo(request):
-    name = "Dismas"
-    html = "<html><body><h1>Hello World! from %s</h1></body></html>" % name
-    return HttpResponse(html)
+from django.shortcuts import render_to_response
+def foo(request,):
+    return render_to_response("helloDJ/home.html",
+                               {"Testing" : "Django Template Inheritance ",
+                               "HelloHello" : "Hello World - Dismas"})
