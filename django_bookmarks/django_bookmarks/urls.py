b95e0749738a764428c69b4f7ad3c7c4ca70c713
ImayaDismas <imayadismas@gmail.com>
2016-03-17 06:48:09 +0300

    Added the view and the url

--- django_bookmarks/django_bookmarks/urls.py
+++ django_bookmarks/django_bookmarks/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+# from django.conf.urls import url
+# from django.contrib import admin
+#
+# urlpatterns = [
+#     url(r'^$', admin.site.urls),
+# ]
+from django.conf.urls.defaults import *
+from bookmarks.views import *
+urlpatterns = ('',
+(r'^$', main_page),
+)
